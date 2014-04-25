fcf6584aaf97134564b20c300d0afa4da83d175e
Ravi Desai <ravi@ravidesai.com>
2014-04-25 16:20:17 -0400

    Figured out personal access tokens with Adam
    - Using personal access tokens since Status API uses REST interface
    - TODO : Push status of commit using STATUS API
    - Regexes for "binding.pry" are working.  Tested with one PR.

--- hooks.js
+++ hooks.js
@@ -4,6 +4,7 @@
 var http = require("http");
 var https = require("https");
 var exec = require("child_process").exec;
+var url = require("url");
 
 http.createServer(function(request, response) {
   if (request.method == "POST") {
@@ -14,31 +15,83 @@ http.createServer(function(request, response) {
       data += chunk;
     });
     request.on("end", function() {
-      var postBody = JSON.parse(data);
-      shaOfLastCommit = postBody.commits[postBody.commits.length-1].sha;
-      console.log(shaOfLastCommit);
+      data = JSON.parse(data);
+      var shaOfLastCommit = null;
+      if (data.action != "closed") {
+	var hostname = url.parse(data.pull_request.commits_url).host
+	var path = url.parse(data.pull_request.commits_url).pathname + "?access_token=" + process.env.BUGGER_PERSONAL_ACCESS_TOKEN
+	https.get({
+	  hostname: hostname
+	  ,path: path
+	  ,headers: {"User-Agent": "Mozilla/5.0"}
+	}, function(res){
+	  var data = "";
+	  res.on("data", function(chunk){
+	    data += chunk;
+	  });
+	  res.on("end", function(){
+	    data = JSON.parse(data);
+	    shaOfLastCommit = data[data.length - 1].sha
+	  })
+	});
+	matches_found(data, update_status);
+      }
     });
 
-    if (data.action != "closed") {
-      var diff = "";
-//      https.get(data.pull_request.diff_url, function(res) {
-      https.get("https://github.com/enginuitygroup/street-smart/pull/734.diff", function(res) {
-	res.on("data", function(chunk) {
-	  diff += chunk;
-	});
-	res.on("end", function() {
-	  console.log(diff);
-	  if (/^\+.*binding.pry/.test(diff)) {
-	    console.log("NO matches found");
-	  } else {
-	    console.log("Matches found");
-	  }
-	});
-      });
-    }
-
     response.end();
-
-
   }
 }).listen(8888);
+
+function matches_found(webhook_payload, update_status_callback){
+  var diff_json = "";
+  var path = "/repos/enginuitygroup/street-smart/compare/staging..." + webhook_payload.pull_request.head.ref + "?access_token=" + process.env.BUGGER_PERSONAL_ACCESS_TOKEN
+  https.get({
+    hostname: "api.github.com"
+    ,path: path
+    ,headers: {"User-Agent": "Mozilla/5.0"}
+  },function(res) {
+    res.on("data", function(chunk) {
+      diff_json += chunk;
+    });
+    res.on("end", function() {
+      var numberOfMatchesFound = 0;
+      diff_json = JSON.parse(diff_json);
+      diff_json.files.forEach(function(element) {
+	if (/\+.*binding\.pry/m.test(element.patch)) {
+	  numberOfMatchesFound++;
+	}
+      });
+
+      var match_found = false;
+      if (numberOfMatchesFound > 0) {
+	match_found = true;
+      }
+
+      update_status_callback(match_found);
+
+    });
+  }).on("error", function(e){
+    console.error(e)
+  });
+}
+
+function update_status(match_found) {
+  var state;
+  var description;
+  if (match_found == true){
+    state = "failure";
+    description = "The Bugger found a match for 'binding.pry'"
+  } else {
+    state = "success";
+    description = "The Bugger found no match for 'binding.pry'"
+  };
+
+  var status = {
+    state: state
+    ,description: description
+    ,context: "Bugger status"
+  }
+console.log(status);
+
+
+}
