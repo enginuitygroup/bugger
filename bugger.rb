e7141252b2a05e38f3fd82918877a4cd8257e1bc
Ravi Desai <ravi@ravidesai.com>
2014-09-14 13:13:24 -0400

    Use a config.json file
    - example provided in repo.

--- bugger.rb
+++ bugger.rb
@@ -3,8 +3,6 @@ require "json"
 require "pry"
 require "sinatra/reloader" if development?
 require "faraday"
-require "dotenv"
-Dotenv.load
 
 set :port, 9494
 set :environment, :production
@@ -27,16 +25,24 @@ post "/payload" do
   end
 end
 
+def bugger_watch_list
+  JSON.parse(File.read("config.json"))["watch_list"]
+end
+
+def bugger_personal_access_token
+  JSON.parse(File.read("config.json"))["personal_access_token"]
+end
+
 def matches_found? webhook_payload, sha_of_last_commit
   puts "Looking for matches in commit #{sha_of_last_commit}"
   path = webhook_payload[:pull_request][:head][:repo][:compare_url].
     gsub("{base}", webhook_payload[:pull_request][:head][:repo][:default_branch]).
     gsub("{head}", webhook_payload[:pull_request][:head][:ref])
-  regexes = Regexp.union JSON.parse(File.read(ENV["BUGGER_WATCH_LIST"]))
+  regexes = Regexp.union bugger_watch_list
   final_regex = /\+.*#{regexes}/
   number_of_matches = 0
 
-  diff = JSON.parse(Faraday.get(path, {access_token: ENV["BUGGER_PERSONAL_ACCESS_TOKEN"]}).body, symbolize_names: true)
+  diff = JSON.parse(Faraday.get(path, {access_token: bugger_personal_access_token}).body, symbolize_names: true)
 
   diff[:files].each do |file|
     if final_regex =~ file[:patch]
@@ -69,7 +75,7 @@ def update_status match_found, statuses_url, sha_of_last_commit
 
   post_response = Faraday.post(statuses_url.gsub("{sha}", sha_of_last_commit)) do |req|
 
-    req.params = {access_token: ENV["BUGGER_PERSONAL_ACCESS_TOKEN"]}
+    req.params = {access_token: bugger_personal_access_token}
     req.body = JSON.dump(status)
   end
 end
